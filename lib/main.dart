9fcef149b7c73b75adc148c246d173d4f0df2586
Elmaghraby <mahmoudelmaghraby515@gmail.com>
2020-05-22 08:06:58 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  var questions = [
+    'What\'s your favorite color?',
+    'What\'s your favorite animal?'
+  ];
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz app'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text('The questions'),
+            RaisedButton(
+              onPressed: (){},
+              child: Text('Answer 1'),
+            ),
+            RaisedButton(
+              onPressed: (){},
+              child: Text('Answer 2'),
+            ),
+            RaisedButton(
+              onPressed: (){},
+              child: Text('Answer 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

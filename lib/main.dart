02bbda3e4106aa769016806994b95d6866e1bb7e
Elmaghraby <mahmoudelmaghraby515@gmail.com>
2020-05-23 06:43:00 +0200

    Third commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,29 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    
+    setState(() {
+      _questionIndex += 1;
+    });
+    
+  }
+
   @override
   var questions = [
     'What\'s your favorite color?',
     'What\'s your favorite animal?'
   ];
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -16,17 +33,19 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Text('The questions'),
+            Text(
+              questions[_questionIndex],
+            ),
             RaisedButton(
-              onPressed: (){},
+              onPressed: _answerQuestion,
               child: Text('Answer 1'),
             ),
             RaisedButton(
-              onPressed: (){},
+              onPressed: _answerQuestion,
               child: Text('Answer 2'),
             ),
             RaisedButton(
-              onPressed: (){},
+              onPressed: _answerQuestion,
               child: Text('Answer 3'),
             ),
           ],
